c1c7c2727ddb387badf92d81fd6263a5d5638177
lovdeep7 <lovdeep7@gmail.com>
2020-07-05 19:57:43 -0400

    Updates

---
+++ precache-manifest.58a4687557e9bd97fe33e619194c4768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9a0f4e80b94075007c312025964416",
+    "url": "/brainfreeze-visualizer/index.html"
+  },
+  {
+    "revision": "6251c8785ff144f43ec7",
+    "url": "/brainfreeze-visualizer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd2b18fb2bad2807171f",
+    "url": "/brainfreeze-visualizer/static/css/main.338b9583.chunk.css"
+  },
+  {
+    "revision": "6251c8785ff144f43ec7",
+    "url": "/brainfreeze-visualizer/static/js/2.63b808c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/brainfreeze-visualizer/static/js/2.63b808c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2b18fb2bad2807171f",
+    "url": "/brainfreeze-visualizer/static/js/main.ac4e380a.chunk.js"
+  },
+  {
+    "revision": "1ce354571c365b796ede",
+    "url": "/brainfreeze-visualizer/static/js/runtime-main.a0b544cb.js"
+  }
+]);
